caa957004cef238239f6549fcf587db997feb442
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-09-06 12:04:54 +0100

    Update detectPanelOverlap.m

--- detectPanelOverlap.m
+++ detectPanelOverlap.m
@@ -1,3 +1,6 @@
+%% Detect Panel Overlap, following twitter feed:
+%https://twitter.com/MicrobiomDigest/status/1433512130406408194
+
 %% Read image
 % read directly from the twitter feed
 test_image=imread('https://pbs.twimg.com/media/E-Tbo4sXMAgQt-0?format=jpg&name=large');
@@ -31,11 +34,11 @@ clear cross_corr_panels new_image
 new_image(rows,cols,numPanels)=0;
 %% Obtain the cross correlation of all panels
 % there may be a better way to run this but for the time it works
-for counter1=10%:numPanels
+for counter1=1:numPanels
     coords_1 = ceil(panel_boundingBox(counter1).BoundingBox);
     rows_1   = coords_1(2)+5:min(rows,coords_1(2)+minBoxes(4)-5);
     cols_1   = coords_1(1)+5:min(cols,coords_1(1)+minBoxes(3)-5);
-    for counter2=27%1:numPanels
+    for counter2=1:numPanels
         disp([counter1 counter2])
         coords_2 = ceil(panel_boundingBox(counter2).BoundingBox);
         rows_2   = coords_2(2)+5:coords_2(2)+minBoxes(4)-5;
@@ -47,18 +50,18 @@ for counter1=10%:numPanels
             cross_corr_panels(2:2:end,2:2:end,counter1,counter2);
     end
 end
-% individual matches can be visualised
-figure(1)
-subplot(131)
-imagesc(edges_panels(rows_1,cols_1));
-subplot(132)
-imagesc(edges_panels(rows_2,cols_2));
-subplot(133)
-mesh(new_image(rows_2,cols_2,counter1));colorbar
-colormap hot
-    axis ij
-
-    axis tight
+% % individual matches can be visualised
+% figure(1)
+% subplot(131)
+% imagesc(edges_panels(rows_1,cols_1));
+% subplot(132)
+% imagesc(edges_panels(rows_2,cols_2));
+% subplot(133)
+% mesh(new_image(rows_2,cols_2,counter1));colorbar
+% colormap hot
+%     axis ij
+% 
+%     axis tight
 %% display as a montage of all correlations
 
 pos1=reshape([1:28],4,7);
@@ -75,15 +78,59 @@ for kk=1:numPanels
     axis tight
 end
     colormap hot
+%% Change dimensions for better visualisation
+for kk=1:numPanels
+    %h{kk}.Position(1)=h{kk}.Position(1)+0.02;
+    %h{kk}.Position(2)=h{kk}.Position(2)-0.02;
+    h{kk}.Position(3)=0.09;
+    h{kk}.Position(4)=0.19;
+    %h{kk}.Visible='on';
+    %h{kk}.Color='k';
+end
+    
+    
+%% Display results as plots that show the cross correlation between panels
+figure(9)
+clf
+for kk=1:numPanels
+    disp(kk)
+    currentPanelData        = cross_corr_panels(:,:,kk,:);
+    currentPanelPeaks       = squeeze(max(max(currentPanelData)));
+    currentPanelMainPeak    = currentPanelPeaks(kk);
+    currentPanelOtherPeaks  = currentPanelPeaks(setdiff(1:numPanels,kk));
+    meanOtherPeaks          = mean(currentPanelOtherPeaks);
+    stdOtherPeaks           = std(currentPanelOtherPeaks);
+    upperLimit              = meanOtherPeaks+3*stdOtherPeaks;
+    peaksAboveUpper         = find(currentPanelPeaks>upperLimit);
+    
+    peaksAboveUpper_L       = find(currentPanelPeaks>upperLimit);
+    peaksAboveUpper_V       = currentPanelPeaks(peaksAboveUpper_L);
+    
+    h{kk}=subplot(4,7, pos2(kk));
+    %kk=21;
+    
+    plot(1:numPanels,currentPanelPeaks,'k',kk,currentPanelMainPeak,'b*',[1 numPanels],[upperLimit upperLimit],'r:',peaksAboveUpper_L,peaksAboveUpper_V,'dm')
+    %axis ij
+    %axis off
+    if numel(peaksAboveUpper_L)>1
+        title(num2str(peaksAboveUpper_L),'fontsize',11)
+    else
+        title(num2str(peaksAboveUpper_L),'fontsize',9)
+    end
+    axis tight
+    set(gca,'yscale','linear')
+    grid on
+end
+
 
 %% Change dimensions for better visualisation
 for kk=1:numPanels
     %h{kk}.Position(1)=h{kk}.Position(1)+0.02;
     %h{kk}.Position(2)=h{kk}.Position(2)-0.02;
-    h{kk}.Position(3)=0.1;
-    h{kk}.Position(4)=0.22;
-    h{kk}.Visible='on';
-    h{kk}.Color='k';
+    h{kk}.Position(3)=0.09;
+    h{kk}.Position(4)=0.19;
+    %h{kk}.Visible='on';
+    %h{kk}.Color='k';
 end
 
 
